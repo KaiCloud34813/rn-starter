23d0414c908d3a3525d87e2e12911a99f3d91395
KaiCloud34813 <anass.abounouh@gmail.com>
2023-04-29 07:36:03 -0500

    destructuring props

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,23 +2,22 @@ import React from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 import { navigator } from "../../App.js";
 
-const HomeScreen = (props) => {
+const HomeScreen = ({ navigation }) => {
   return (
     <View>
       <Text style={styles.text}> Sandara App</Text>
       <Button
         title="go to Components"
         onPress={() => {
-          props.navigation.navigate("Components");
+          navigation.navigate("Components");
         }}
       />
-      <TouchableOpacity
+      <Button
+        title="go to List"
         onPress={() => {
-          props.navigation.navigate("List");
+          navigation.navigate("List");
         }}
-      >
-        <Text>Go to SandaraList</Text>
-      </TouchableOpacity>
+      />
     </View>
   );
 };
