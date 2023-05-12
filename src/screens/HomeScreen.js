e2d8ada782a7c846f26cb4a42a76c2ee0845299e
KaiCloud34813 <anass.abounouh@gmail.com>
2023-05-11 22:19:43 -0500

    ...some styling

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -47,6 +47,12 @@ const HomeScreen = ({ navigation }) => {
           navigation.navigate("Text");
         }}
       />
+      <Button
+        title="go to Box"
+        onPress={() => {
+          navigation.navigate("Box");
+        }}
+      />
     </View>
   );
 };
